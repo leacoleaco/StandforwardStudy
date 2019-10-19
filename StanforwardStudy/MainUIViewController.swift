5636d5103c3740d4630189f93ff10e0eae85b924
Leaco <leaco9012@163.com>
2019-10-19 20:27:00 +0800

    学习第一章

--- StanforwardStudy/MainUIViewController.swift
+++ StanforwardStudy/MainUIViewController.swift
@@ -9,22 +9,38 @@
 import UIKit
 
 class MainUIViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var filpCount=0{
+        didSet{
+            filpCountLabel.text="Filps:\(filpCount)"
+        }
     }
     
+    var emojyChoices = ["😈","😈","🎃","🎃","😈","🎃"]
 
-    /*
-    // MARK: - Navigation
+    @IBOutlet weak var filpCountLabel: UILabel!
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        filpCount += 1
+        let index: Int? = cardButtons.firstIndex(of: sender)
+//        print("cardNo:\(index)")
+        filpCard(withEmojy: emojyChoices[index!], on: sender)
     }
-    */
+
+    func filpCard(withEmojy emojy:String,on button:UIButton){
+        
+        print("filp with:\(emojy)")
+        
+        if button.currentTitle==emojy{
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor=#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        }else{
+            button.setTitle(emojy, for: UIControl.State.normal)
+            button.backgroundColor=#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
+    }
 
 }
